349f933aa7653d1c462f6c035b1510f21d38b3d7
You Zhou <53392585+ZhouYou-NJU@users.noreply.github.com>
2023-09-11 20:56:53 +0800

    Add files via upload

--- header-main.js
+++ header-main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-document.getElementById('header-main').innerHTML= `
+document.getElementById('header-main').innerHTML = `
 <div>
   <h1>
     <div>
@@ -34,12 +34,12 @@ document.getElementById('header-main').innerHTML= `
           </tr>
           <tr>
             <td width="5%" bgcolor="#F2F1EB" style="text-align: center">&nbsp;</td>
-            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><span style="color: #000000"><a href="index.html" style="font-size: 22px">研究首页</a></span></td>
-            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/reseaches.html" style="font-size: 22px">研究领域</a></td>
-            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/members.html" style="font-size: 22px">团队成员</a></td>
-            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/publications.html" style="font-size: 22px">发表论文</a></td>
-            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/education.html" style="font-size: 22px">人才培养</a></td>
-            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/news.html" style="font-size: 22px">新闻速递</a></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><span style="color: #000000"><a href="/index.html" style="font-size: 22px">研究首页</a></span></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="/GroupPageColumn/reseaches.html" style="font-size: 22px">研究领域</a></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="/GroupPageColumn/members.html" style="font-size: 22px">团队成员</a></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="/GroupPageColumn/publications.html" style="font-size: 22px">发表论文</a></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="/GroupPageColumn/education.html" style="font-size: 22px">人才培养</a></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="/GroupPageColumn/news.html" style="font-size: 22px">新闻速递</a></td>
             <td width="5%" bgcolor="#F2F1EB" style="font-family: '微软雅黑'"></td>
           </tr>
           <tr>
