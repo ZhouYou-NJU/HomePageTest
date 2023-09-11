3ccda1356d31a46771061701c26ee39d16b361b6
You Zhou <53392585+ZhouYou-NJU@users.noreply.github.com>
2023-09-11 17:33:32 +0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,60 @@
+document.getElementById('header').innerHTML = `
+<div>
+  <h1>
+    <div>
+      <div>
+        <table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+          <tr bgcolor="#B1040E">
+            <td height="10" colspan="2"></td>
+          </tr>
+          <tr>
+            <td width="5%" bgcolor="#B1040E"></td>
+            <td valign="baseline" bgcolor="#B1040E" style="text-align: left; font-size: 28px; font-family: Georgia, 'Times New Roman', Times, serif; color: #FFFFFF;">
+              <p><strong>南京大学工程医学研究组</strong>（EngMed Group@NJU）</p>
+              <p>Engineering Medicine Research Group, Nanjing University</p>
+            </td>
+          </tr>
+          <tr bgcolor="#B1040E">
+            <td height="10" colspan="4"></td>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="5%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="5%" height="5" bgcolor="#F2F1EB"></td>
+          </tr>
+          <tr>
+            <td width="5%" bgcolor="#F2F1EB" style="text-align: center">&nbsp;</td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><span style="color: #000000"><a href="index.html" style="font-size: 22px">研究首页</a></span></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/reseaches.html" style="font-size: 22px">研究领域</a></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/members.html" style="font-size: 22px">团队成员</a></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/publications.html" style="font-size: 22px">发表论文</a></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/education.html" style="font-size: 22px">人才培养</a></td>
+            <td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/news.html" style="font-size: 22px">新闻速递</a></td>
+            <td width="5%" bgcolor="#F2F1EB" style="font-family: '微软雅黑'"></td>
+          </tr>
+          <tr>
+            <td width="5%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="15%" height="5" bgcolor="#F2F1EB"></td>
+            <td width="5%" height="5" bgcolor="#F2F1EB"></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </h1>
+</div>
+`;
+// JavaScript Document
