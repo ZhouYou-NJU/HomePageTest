b7bf9182f9bdbe326bde6d12d1f25e5901dbf385
You Zhou <53392585+ZhouYou-NJU@users.noreply.github.com>
2023-09-11 22:06:58 +0800

    Add files via upload

--- header-news.js
+++ header-news.js
@@ -8,7 +8,7 @@ document.getElementById('header-news').innerHTML = `
         </tr>
         <tr>
           <td width="5%" bgcolor="#B1040E"></td>
-          <td valign="baseline" bgcolor="#B1040E" style="text-align: left; font-size: 28px; font-family: Georgia, 'Times New Roman', Times, serif; color: #FFFFFF;">
+          <td valign="baseline" bgcolor="#B1040E" style="text-align: left; font-size: 24px; font-family: Georgia, 'Times New Roman', Times, serif; color: #FFFFFF;">
             <p><strong>南京大学工程医学研究组</strong>（EngMed Group@NJU）</p>
             <p>Engineering Medicine Research Group, Nanjing University</p>
           </td>
@@ -19,36 +19,36 @@ document.getElementById('header-news').innerHTML = `
       </table>
     </div>
     <table width="100%" border="0" cellpadding="0" cellspacing="0">
-      <tr>
-        <td height="5" bgcolor="#F2F1EB"></td>
-        <td height="5" bgcolor="#F2F1EB"></td>
-        <td width="15%" height="15" bgcolor="#F2F1EB"></td>
-        <td width="15%" height="15" bgcolor="#F2F1EB"></td>
-        <td width="15%" height="15" bgcolor="#F2F1EB"></td>
-        <td width="15%" height="15" bgcolor="#F2F1EB"></td>
-        <td height="15" bgcolor="#F2F1EB"></td>
-        <td height="15" bgcolor="#F2F1EB"></td>
-      </tr>
-      <tr>
-        <td width="5%" height="5" bgcolor="#F2F1EB" style="font-family: '微软雅黑'; font-size: 24px;">&nbsp;</td>
-        <td width="15%" bgcolor="#F2F1EB" style="font-family: '微软雅黑'; font-size: 24px;"><span style="color: #000000"><a href="GroupPageColumn/news.html">研究组新闻</a></span></td>
-        <td width="15%" height="5" bgcolor="#F2F1EB" style="font-family: '微软雅黑'"><a href="GroupPageColumn/progress.html" style="font-size: 24px">研究进展</a></td>
-        <td width="15%" height="5" bgcolor="#F2F1EB" style="font-family: '微软雅黑'"><a href="GroupPageColumn/informal essay.html" style="font-size: 24px">动态与随想</a></td>
-        <td width="15%" height="5" bgcolor="#F2F1EB" style="font-family: '微软雅黑'"><a href="index.html" style="font-size: 24px">返回首页</a></td>
-        <td width="15%" height="15" bgcolor="#F2F1EB" style="font-family: '微软雅黑'">&nbsp;</td>
-        <td width="15%" height="5" bgcolor="#F2F1EB" style="font-family: '微软雅黑'">&nbsp;</td>
-        <td width="5%" bgcolor="#F2F1EB" style="font-family: '微软雅黑'; font-size: 24px;">&nbsp;</td>
-      </tr>
-      <tr>
-        <td height="5" bgcolor="#F2F1EB"></td>
-        <td height="5" bgcolor="#F2F1EB"></td>
-        <td width="15%" height="15" bgcolor="#F2F1EB"></td>
-        <td width="15%" height="15" bgcolor="#F2F1EB"></td>
-        <td width="15%" height="15" bgcolor="#F2F1EB"></td>
-        <td width="15%" height="15" bgcolor="#F2F1EB"></td>
-        <td height="15" bgcolor="#F2F1EB"></td>
-        <td height="15" bgcolor="#F2F1EB"></td>
-      </tr>
+	  <tr>
+		<td width="5%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="5%" height="5" bgcolor="#F2F1EB"></td>
+	  </tr>
+	  <tr>
+		<td width="5%" bgcolor="#F2F1EB" style="text-align: center">&nbsp;</td>
+		<td width="15%" bgcolor="#F2F1EB" style="text-align: center"><span style="color: #000000"><a href="GroupPageColumn/news.html" style="font-size: 22px">研究组新闻</a></span></td>
+		<td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/progress.html" style="font-size: 22px">研究进展</a></td>
+		<td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="GroupPageColumn/informal essay.html" style="font-size: 22px">动态与随想</a></td>
+		<td width="15%" bgcolor="#F2F1EB" style="text-align: center"><a href="index.html" style="font-size: 22px">返回首页</a></td>
+		<td width="15%" bgcolor="#F2F1EB" style="text-align: center">&nbsp;</td>
+		<td width="15%" bgcolor="#F2F1EB" style="text-align: center">&nbsp;</td>
+		<td width="5%" bgcolor="#F2F1EB" style="font-family: '微软雅黑'">&nbsp;</td>
+	  </tr>
+	  <tr>
+		<td width="5%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="5%" height="5" bgcolor="#F2F1EB"></td>
+	  </tr>
     </table>
   </div>
 `;
